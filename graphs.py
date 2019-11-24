1c261ddfd836a8053e80a44c260a9e8bf32a5d16
ShamanEstBanan <kjldfgjsdkflg>
2019-11-24 13:59:58 +0300

    third commit

--- graphs.py
+++ graphs.py
@@ -25,7 +25,77 @@ def depth_pyhton(graph, start, target):
             if v == target:
                 return True
             elif v not in visited:
-                stack.append(0, v)
+                stack.append(v)
+    return False
+
+
+def depth_recursive(graph, start, target, visited=None):
+    print("depth_recursive current: ", start)
+    if start == target:
+        return True
+    if visited is None:
+        visited = set()
+    visited.add(start)
+
+    for vertex, value in enumerate(graph[start]):
+        if value == 1:
+            if vertex == target or (vertex not in visited and depth_recursive(graph, vertex, target, visited)):
+                return True
+    return False
+
+
+def depth(graph, start, target):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        print("depth current: ", vertex)
+        visited.add(vertex)
+        if vertex == target:
+            return True
+        for v, value in enumerate(graph[vertex]):
+            if value == 1:
+                if v == target:
+                    return True
+            elif v not in visited:
+                stack.append(v)
+    return False
+
+
+def width_python(graph, start, target):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop()
+        print("width current: ", vertex)
+        visited.add(vertex)
+        if vertex == target:
+            return True
+
+        for v in graph[vertex]:
+            if v == target:
+                return True
+            elif v not in visited and v not in queue:
+                queue.insert(0, v)
+    return False
+
+
+def width(graph, start, target):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop()
+        print("width current: ", vertex)
+        visited.add(vertex)
+        if vertex == target:
+            return True
+
+        for v, value in enumerate(graph[vertex]):
+            if value == 1:
+                if v == target:
+                    return True
+                elif v not in visited and v not in queue:
+                    queue.insert(0, v)
     return False
 
 
@@ -39,9 +109,27 @@ graph_pyton = {
     "G": ("D", "E", "F"),
 }
 
+graph = (
+    (0, 1, 0, 0, 0, 0, 0),
+    (0, 0, 1, 0, 0, 0, 0),
+    (1, 0, 0, 1, 0, 0, 0),
+    (0, 1, 0, 0, 0, 0, 0),
+    (0, 0, 1, 0, 0, 0, 0),
+    (0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 0, 1, 1, 1, 0),
+)
+
 print(depth_python_recursive(graph_pyton, "A", "F"))
 print(depth_pyhton(graph_pyton, "A", "F"))
 
-
 print(depth_python_recursive(graph_pyton, "G", "A"))
 print(depth_pyhton(graph_pyton, "G", "A"))
+
+print(depth_recursive(graph, 6, 0))
+print(depth(graph, 6, 0))
+
+print(width_python(graph_pyton, "G", "A"))
+print(width_python(graph_pyton, "A", "F"))
+
+
+print(width(graph, 6, 0))
